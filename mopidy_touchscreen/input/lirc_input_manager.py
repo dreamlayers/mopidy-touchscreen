a39f0be475958edc22aa32d892bb38690c10b6e2
Boris Gjenero <boris.gjenero@gmail.com>
2017-12-11 04:33:16 -0500

    Simplify LIRC code and ignore repeats in a better way

--- mopidy_touchscreen/input/lirc_input_manager.py
+++ mopidy_touchscreen/input/lirc_input_manager.py
@@ -1,44 +1,20 @@
 import socket
-from datetime import datetime
 
-class lircsource(object):
+class LIRCManager():
     def __init__(self, fname = '/var/run/lirc/lircd'):
         self.sock = socket.socket(socket.AF_UNIX)
         self.sock.connect(fname)
         self.sock.setblocking(0)
-        self.lastkey = None
 
-    def getraw(self):
+    def get(self):
         try:
             s = self.sock.recv(4096)
         except:
             return []
         res = []
         for l in s.splitlines():
-            res.append(l.split(' ')[2])
+            ldata = l.split(' ')
+            # Ignore repeats
+            if int(ldata[1]) == 0:
+                res.append(ldata[2])
         return res
-
-    def get(self):
-        res = []
-        keys = self.getraw()
-        now = datetime.now()
-        mayrepeat = self.lastkey is not None and \
-                    (now-self.lktime).total_seconds() < 0.5
-        if len(keys) > 0:
-            self.lktime = now
-        for k in keys:
-            if mayrepeat:
-                if k == self.lastkey:
-                    continue
-            else:
-                mayrepeat = True
-            res.append(k)
-            self.lastkey = k
-        return res
-
-class LIRCManager():
-    def __init__(self):
-        self.lirc = lircsource()
-
-    def get(self):
-        return self.lirc.get()
